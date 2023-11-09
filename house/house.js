a9e44d9e9c2b623004903e3ffcf9f99f746174eb
aay074 <klaus.juenemann@haw-hamburg.de>
2023-11-09 14:07:26 +0100

    house exercise after chapter 5

--- house/house.js
+++ house/house.js
@@ -1,6 +1,6 @@
 // A house
 
-import * as THREE from "three"
+import * as THREE from "three";
 import {TrackballControls} from "three/addons/controls/TrackballControls.js";
 
 // Initialize WebGL renderer
@@ -28,8 +28,39 @@ const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(ambientLight);
 
 // Add the ground
+const groundMat = new THREE.MeshPhongMaterial({color: "green",
+                                               side:THREE.DoubleSide} );
+groundMat.transparent = true;
+groundMat.opacity = 0.5;
+const groundGeo = new THREE.PlaneGeometry(20,20);
+const ground = new THREE.Mesh(groundGeo, groundMat);
+ground.rotation.x = -Math.PI/2;
+scene.add(ground);
 
 // Build the house
+const house = new THREE.Object3D(); 
+scene.add(house);
+// window.house = house;
+house.position.x = 2; 
+
+const width = 1;
+const len = 2;
+const height = 0.8;
+
+const bodyMat = new THREE.MeshPhongMaterial({color: "gray"} );
+
+const bodyGeo = new THREE.BoxGeometry(width,height,len);
+const body = new THREE.Mesh(bodyGeo, bodyMat);
+body.position.y = height / 2 + 0.0001;
+house.add(body);
+
+// Add the roof to the house
+const roofGeo = createIndexedRoofGeo(1.1*len,1.1*width,0.5*height);
+const roofMat = new THREE.MeshPhongMaterial({color: 0xff4444});
+const roof = new THREE.Mesh(roofGeo, roofMat);
+
+roof.position.y = height;
+house.add(roof);
 
 
 // Render loop
@@ -41,3 +72,30 @@ function render() {
 }
 
 render();
+
+
+function createIndexedRoofGeo(l, w, h) {
+
+  const vertices = new Array(6);
+  vertices[0] = new THREE.Vector3(-w/2, 0, -l/2);
+  vertices[1] = new THREE.Vector3(w/2, 0, -l/2);
+  vertices[2] = new THREE.Vector3(0, h, -l/2);
+  vertices[3] = new THREE.Vector3(-w/2, 0, l/2);
+  vertices[4] = new THREE.Vector3(w/2, 0, l/2);
+  vertices[5] = new THREE.Vector3(0, h, l/2);
+
+  const indices = [0, 2, 1,
+                   3, 4, 5,
+                   0, 3, 2,
+                   2, 3, 5,
+                   0, 1, 4,
+                   0, 4, 3,
+                   1, 2, 5,
+                   1, 5, 4];
+
+  const geo = new THREE.BufferGeometry();
+  geo.setFromPoints(vertices);
+  geo.setIndex(indices);
+
+  return geo;
+}
